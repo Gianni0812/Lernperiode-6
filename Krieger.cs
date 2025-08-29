73ed3cab5dd60685c2e65f4971796c604b528d78
Gianni0812 <g.caruso.inf24@stud.bbbaden.ch>
2025-08-29 10:44:17 +0200

    Add files via upload

---
+++ Krieger.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lernperiode_6
+{
+    internal class Krieger
+    {
+        public string Catchpharse { get; set; } = "Sehr erfreut";
+
+        public void Vorstellen(int anzahl)
+        {
+            for(int i = 0;  i < anzahl; i++)
+            {
+                if (i < anzahl)
+                {
+                    Console.WriteLine("Mein Name ist:" +Catchpharse);
+                }
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿using Lernperiode_6;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace Lernperiode6
+{
+  
+    public class Program
+    {
+
+        public static void Main(string[] args)
+        {
+            Krieger kriegerMartin = new Krieger();
+            kriegerMartin.Catchpharse = "Martin";
+            kriegerMartin.Vorstellen(1);
+
+            Krieger kriegerinLuana = new Krieger();
+            kriegerinLuana.Catchpharse = "Luna";
+            kriegerinLuana.Vorstellen(1);
+            Console.WriteLine("");
+            Console.WriteLine("Wähle einen Krieger aus:");
+            string player = "";
+            bool weiter = true;
+            do
+            {
+                string playerAuswahl = Console.ReadLine().ToLower();
+                if (playerAuswahl == "luna" || playerAuswahl == "martin")
+                {
+                    player = playerAuswahl;
+                    weiter = false;
+                }
+                else
+                {
+                    Console.WriteLine("Fehler bei der Eingabe! Stellen Sie sicher das Sie Luna oder Martin eingebn!");
+                }
+
+            } while (weiter);
+
+            if (player == "martin")
+            {
+                Waffen Schwert = new Waffen();
+                Waffen Axt = new Waffen();
+                Waffen Hammer = new Waffen();
+            }
+            else
+            {
+                Waffen Bogen = new Waffen();
+                Waffen Speer = new Waffen();
+                Waffen Armbrust = new Waffen();
+            }
+            
+
+
+        }
+    }
+}
